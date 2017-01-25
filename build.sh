2544df3808d133f1cf676e91531b592ce6853b26
James Nocentini <james.nocentini@gmail.com>
2017-01-25 14:48:48 +0000

    parameterize script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-TMP="tmp"
+TMP="${1}"
 
 rm -rf tmp
 mkdir tmp
@@ -14,15 +14,17 @@ mono ~/Developer/GitHubReleaseNotes/src/App/bin/Debug/ReleaseNotesCompiler.CLI.e
 mono ~/Developer/GitHubReleaseNotes/src/App/bin/Debug/ReleaseNotesCompiler.CLI.exe create --owner couchbase --repository couchbase-lite-net --targetcommitish master -u jamiltz -p Robobo12! -m 1.4.0 --exportmd
 mono ~/Developer/GitHubReleaseNotes/src/App/bin/Debug/ReleaseNotesCompiler.CLI.exe create --owner couchbase --repository sync_gateway --targetcommitish master -u jamiltz -p Robobo12! -m 1.4.0 --exportmd
 
-echo "Zipping..."
-ditto -c -k --sequesterRsrc --keepParent "tmp" "tmp.zip"
+if [ -z {1} ]; then
+	echo "Zipping..."	
+	ditto -c -k --sequesterRsrc --keepParent "${1}" "tmp.zip"
+	
+	echo "Uploading..."
+	echo "
+	cd uploads/mobile
+	put tmp.zip
+	" > push.sh
 
-echo "Uploading..."
-echo "
-cd uploads/mobile
-put tmp.zip
-" > push.sh
-
-sftp -b push.sh -oIdentityFile=~/.ssh/cb_xfer_id_rsa cb_xfer@54.175.181.113
-
-curl -X POST "http://$JENKINS_USERNAME:$JENKINS_PASSWORD@build-ingestion.cbauthx.com/job/CouchbaseDocumentationJobs/job/Mobile/job/IngestStage/build\?delay\=0sec"
+	sftp -b push.sh -oIdentityFile=~/.ssh/cb_xfer_id_rsa cb_xfer@54.175.181.113
+	
+	curl -X POST "http://$JENKINS_USERNAME:$JENKINS_PASSWORD@build-ingestion.cbauthx.com/job/CouchbaseDocumentationJobs/job/Mobile/job/IngestStage/build\?delay\=0sec"
+fi
