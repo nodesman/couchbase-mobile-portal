94a44156282dbe01e245afbbb77fdc0fc8bb47fd
James Nocentini <james.nocentini@gmail.com>
2017-01-25 12:35:15 +0000

    check in build script

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+TMP="tmp"
+
+rm -rf tmp
+mkdir tmp
+
+echo "Building..."
+
+jekyll build --source "md-docs/" --destination "${TMP}"
+java -jar site/gtor/saxon9.jar -xi -l:on -s:site/src/site-hippo.xml -xsl:site/gtor/hippo.xslt output-directory="${TMP}/"
+
+echo "Zipping..."
+ditto -c -k --sequesterRsrc --keepParent "tmp" "tmp.zip"
+
+echo "Uploading..."
+echo "
+cd uploads/mobile
+put tmp.zip
+" > push.sh
+
+sftp -b push.sh -oIdentityFile=~/.ssh/cb_xfer_id_rsa cb_xfer@54.175.181.113
+
+curl -X POST "http://$JENKINS_USERNAME:$JENKINS_PASSWORD@build-ingestion.cbauthx.com/job/CouchbaseDocumentationJobs/job/Mobile/job/IngestStage/build\?delay\=0sec"
