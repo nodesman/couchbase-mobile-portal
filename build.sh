456e834493c824c37e450a61fa20938d5e72e97d
James Nocentini <james.nocentini@gmail.com>
2017-01-30 16:33:16 +0000

    update build script

--- build.sh
+++ build.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
-TMP="tmp"
+TMP=$1
 
 rm -rf tmp
 mkdir tmp
 
 echo "Building..."
-jekyll build --source "md-docs/" --destination "${TMP}" --config "md-docs/_config.yml","md-docs/_config.${1}.yml"
+jekyll build --source "md-docs/" --destination "${TMP}" --config "md-docs/_config.yml","md-docs/_config.${2}.yml"
 java -jar site/gtor/saxon9.jar -xi -l:on -s:site/src/site-hippo.xml -xsl:site/gtor/hippo.xslt output-directory="${TMP}/"
 
-echo "Zipping..."	
-ditto -c -k --sequesterRsrc --keepParent "${TMP}" "tmp.zip"
-
-echo "Uploading..."
-echo "
-cd uploads/mobile
-put tmp.zip
-" > push.sh
-
-sftp -b push.sh -oIdentityFile=~/.ssh/cb_xfer_id_rsa cb_xfer@54.175.181.113
-
-curl -X POST "http://$JENKINS_USERNAME:$JENKINS_PASSWORD@build-ingestion.cbauthx.com/job/CouchbaseDocumentationJobs/job/Mobile/job/IngestStage/build\?delay\=0sec"
+if [[ ${2} = "upload" ]]; then
+	echo "Zipping..."	
+	ditto -c -k --sequesterRsrc --keepParent "${TMP}" "tmp.zip"
+	echo "Uploading..."
+	echo "
+	cd uploads/mobile
+	put tmp.zip
+	" > push.sh
+	
+	sftp -b push.sh -oIdentityFile=~/.ssh/cb_xfer_id_rsa cb_xfer@54.175.181.113
+	
+	curl -X POST "http://$JENKINS_USERNAME:$JENKINS_PASSWORD@build-ingestion.cbauthx.com/job/CouchbaseDocumentationJobs/job/Mobile/job/IngestStage/build\?delay\=0sec"
+fi
