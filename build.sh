9a9d7865f592b6d0f03b593f0b68d3e22b913116
James Nocentini <james.nocentini@gmail.com>
2017-01-30 16:47:56 +0000

    fix tmp directory

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-TMP=$1
+TMP=tmp
 
 rm -rf tmp
 mkdir tmp
 
 echo "Building..."
-jekyll build --source "md-docs/" --destination "${TMP}" --config "md-docs/_config.yml","md-docs/_config.${2}.yml"
+jekyll build --source "md-docs/" --destination "${TMP}" --config "md-docs/_config.yml","md-docs/_config.${1}.yml"
 java -jar site/gtor/saxon9.jar -xi -l:on -s:site/src/site-hippo.xml -xsl:site/gtor/hippo.xslt output-directory="${TMP}/"
 
 if [[ ${2} = "upload" ]]; then
