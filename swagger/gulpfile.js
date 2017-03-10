84696062c7b9065ae0545ec1c32fa394829cc686
James Nocentini <james.nocentini@gmail.com>
2017-03-10 11:28:44 +0000

    add spec for 1.3 and 1.4.

--- swagger/gulpfile.js
+++ swagger/gulpfile.js
@@ -1,57 +1,59 @@
 var gulp = require('gulp')
   , concat = require('gulp-concat')
   , runSequence = require('run-sequence')
-  , swagger = require('gulp-swagger');
+  , swagger = require('gulp-swagger')
+  , argv = require('yargs').argv
+  , version = argv.version + '/';
 
 /**
  * Concat the parameters into parameters/index.yaml
  */
 gulp.task('params-cbl', function () {
-  return gulp.src(['parameters/common.yaml', 'parameters/cbl.yaml'])
+  return gulp.src([version + 'parameters/common.yaml', version + 'parameters/cbl.yaml'])
     .pipe(concat('index.yaml'))
-    .pipe(gulp.dest('parameters'));
+    .pipe(gulp.dest(version + 'parameters'));
 });
 
 gulp.task('params-sg', function () {
-  return gulp.src(['parameters/common.yaml', 'parameters/sg.yaml'])
+  return gulp.src([version + 'parameters/common.yaml', version + 'parameters/sg.yaml'])
     .pipe(concat('index.yaml'))
-    .pipe(gulp.dest('parameters'));
+    .pipe(gulp.dest(version + 'parameters'));
 });
 
 /**
  * Concat the definitions into definitions/index.yaml
  */
 gulp.task('definitions-cbl', function () {
-  return gulp.src(['definitions/common.yaml', 'definitions/cbl.yaml'])
+  return gulp.src([version + 'definitions/common.yaml', version + 'definitions/cbl.yaml'])
     .pipe(concat('index.yaml'))
-    .pipe(gulp.dest('definitions'));
+    .pipe(gulp.dest(version + 'definitions'));
 });
 
 gulp.task('definitions-sg', function () {
-  return gulp.src(['definitions/common.yaml', 'definitions/sg.yaml'])
+  return gulp.src([version + 'definitions/common.yaml', version + 'definitions/sg.yaml'])
     .pipe(concat('index.yaml'))
-    .pipe(gulp.dest('definitions'));
+    .pipe(gulp.dest(version + 'definitions'));
 });
 
 /**
  * Concat the paths into paths/index.yaml
  */
 gulp.task('paths-cbl', function () {
-  return gulp.src(['paths/common.yaml', 'paths/cbl.yaml'])
+  return gulp.src([version + 'paths/common.yaml', version + 'paths/cbl.yaml'])
     .pipe(concat('index.yaml'))
-    .pipe(gulp.dest('paths'));
+    .pipe(gulp.dest(version + 'paths'));
 });
 
 gulp.task('paths-sg-public', function () {
-  return gulp.src(['paths/common.yaml', 'paths/sg/common.yaml', 'paths/sg/public.yaml'])
+  return gulp.src([version + 'paths/common.yaml', version + 'paths/sg/common.yaml', version + 'paths/sg/public.yaml'])
     .pipe(concat('index.yaml'))
-    .pipe(gulp.dest('paths'));
+    .pipe(gulp.dest(version + 'paths'));
 });
 
 gulp.task('paths-sg-admin', function () {
-  return gulp.src(['paths/common.yaml', 'paths/sg/common.yaml', 'paths/sg/admin.yaml'])
+  return gulp.src([version + 'paths/common.yaml', version + 'paths/sg/common.yaml', version + 'paths/sg/admin.yaml'])
     .pipe(concat('index.yaml'))
-    .pipe(gulp.dest('paths'));
+    .pipe(gulp.dest(version + 'paths'));
 });
 
 /**
@@ -59,22 +61,22 @@ gulp.task('paths-sg-admin', function () {
  */
 
 gulp.task('public', ['params-sg', 'definitions-sg', 'paths-sg-public'], function () {
-  return gulp.src('./public.yaml')
+  return gulp.src(version + 'public.yaml')
     .pipe(swagger('public.json'))
-    .pipe(gulp.dest('./tmp'))
+    .pipe(gulp.dest('tmp'))
 });
 
 gulp.task('admin', ['params-sg', 'definitions-sg', 'paths-sg-admin'], function () {
-  return gulp.src('./admin.yaml')
+  return gulp.src(version + 'admin.yaml')
     .pipe(swagger('admin.json'))
-    .pipe(gulp.dest('./tmp'))
+    .pipe(gulp.dest('tmp'))
 });
 
 gulp.task('cbl', ['params-cbl', 'definitions-cbl', 'paths-cbl'], function () {
-  return gulp.src('./cbl.yaml')
+  return gulp.src(version + 'cbl.yaml')
     .pipe(swagger('cbl.json'))
     // run the swagger js code
-    .pipe(gulp.dest('./tmp'))
+    .pipe(gulp.dest('tmp'))
 });
 
 gulp.task('build', function(done) {
