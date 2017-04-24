126490f653b1d77898877241cf61dcbee5afb342
James Nocentini <james.nocentini@gmail.com>
2017-04-24 15:58:51 +0100

    update build.sh with urls to trigger acct or prod job depending on command argument.

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ echo "Building..."
 jekyll build --source "md-docs/" --destination "${TMP}" --config "md-docs/_config.yml","md-docs/_config.${1}.yml"
 java -jar site/gtor/saxon9.jar -xi -l:on -s:site/src/site-hippo.xml -xsl:site/gtor/hippo.xslt output-directory="${TMP}/"
 
-if [[ ${2} = "upload" ]]; then
+if [[ ! -z ${2} ]]; then
 	echo "Zipping..."	
 	ditto -c -k --sequesterRsrc --keepParent "${TMP}" "tmp.zip"
 	echo "Uploading..."
@@ -19,5 +19,9 @@ if [[ ${2} = "upload" ]]; then
 	
 	sftp -b push.sh -oIdentityFile=~/.ssh/couchbase_sftp.key couchbaseinc@static.hosting.onehippo.com
 	
-	curl -X POST "http://$JENKINS_USERNAME:$JENKINS_PASSWORD@52.200.81.196:8080/job/Ingestion%20Mobile%20-%20PROD%20ENV/build\?delay\=0sec"
+	if [[ ${2} = "acct" ]]; then
+		curl -X POST "http://$JENKINS_USERNAME:$JENKINS_PASSWORD@52.200.81.196:8080/view/Staging%20(Acceptance)%20Content%20Ingestion%20Jobs/job/Ingestion%20Mobile%20-%20ACCT%20ENV/build\?delay\=0sec"
+	elif [[ ${2} = "prod" ]]; then
+		curl -X POST "http://$JENKINS_USERNAME:$JENKINS_PASSWORD@52.200.81.196:8080/job/Ingestion%20Mobile%20-%20PROD%20ENV/build\?delay\=0sec"
+	fi
 fi
